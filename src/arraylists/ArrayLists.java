210e04afcd1ee5ba6f2acff8c6b305a12ef844de
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-01-24 20:22:32 +0000

    Some arraylist methods - add remove get

--- src/arraylists/ArrayLists.java
+++ src/arraylists/ArrayLists.java
@@ -4,9 +4,20 @@
  */
 package arraylists;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author celeb
+ * @author Sam
+ * 
+ * Arrays:
+ *  fixed size
+ *      Need to specify in advance
+ *  store multiple values
+ *  Can store primitive (ie int) as well as objects (ie String, Car)
+ * 
+ * ArrayList
+ *  
  */
 public class ArrayLists {
 
@@ -14,7 +25,44 @@ public class ArrayLists {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] intArray = {1, 2, 3, 4};
+        
+        // Can't have an ArrayList of ints
+//        ArrayList<int> intArrayList;
+
+        // To get around this, we use wrapper class
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        
+//        int num = Integer.parseInt("5") + 8;
+
+        // Adds the number to the end of the list
+        System.out.println(intArrayList.size());
+        intArrayList.add(6);
+        System.out.println(intArrayList.size());
+        intArrayList.add(7);
+        System.out.println(intArrayList.size());
+        intArrayList.add(8);
+        System.out.println(intArrayList.size());
+        
+        // {6, 7, 8}
+        
+        // .add(index, element)
+        intArrayList.add(0, 5);
+        System.out.println(intArrayList.size());
+        
+        // {5, 6, 7, 8}
+        
+        // Remove the 7
+        intArrayList.remove(2);
+        System.out.println(intArrayList.size());
+        
+        // {5, 6, 8}
+        
+        System.out.println(intArrayList.contains(5));
+        System.out.println(intArrayList.contains(7));
+        
+        System.out.println(intArrayList.get(0));
+        
     }
     
 }
