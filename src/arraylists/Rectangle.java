563c5b543d559731d70c1f70d1773155e69b070d
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-01-24 20:34:22 +0000

    Made Rectangle class
    double length
    double width
    String colour
    boolean isFilled;
    
    constructor for all fields
    getters for all fields

---
+++ src/arraylists/Rectangle.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraylists;
+
+/**
+ *
+ * @author celeb
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+    private String colour;
+    private boolean isFilled;
+
+    public Rectangle(double length, double width, String colour, boolean isFilled) {
+        this.length = length;
+        this.width = width;
+        this.colour = colour;
+        this.isFilled = isFilled;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public boolean isIsFilled() {
+        return isFilled;
+    }
+    
+    
+    
+}
